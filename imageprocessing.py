62504736dac2f0e17a2421ba30d0ffc60a7fc777
Allison Chow <allisonchow@ymail.com>
2017-07-19 12:19:27 -0700

    Successfully finds 1 circle and center of sun

--- imageprocessing.py
+++ imageprocessing.py
@@ -3,6 +3,11 @@ import cv2
 import numpy as np
 
 
+"""
+Binary image
+"""
+
+
 # Open image as PIL image object
 img = Image.open('sky1.jpg')
 
@@ -16,7 +21,7 @@ for i in range(0,img.width):
 	for j in range(0,img.height):
 
 		# If red component is less than 0.8*255, then make pixel white
-		if (img.getpixel((i,j))[0] < 204): 
+		if (img.getpixel((i,j))[0] < 250): 
 			bwimg.putpixel((i,j), 0)
 		
 		# If red component is greater than 0.8*255, then make pixel black 
@@ -25,32 +30,56 @@ for i in range(0,img.width):
 
 
 # Save image
-bwimg.save('binaryimage.png')
+bwimg.save('binaryimage.jpg')
+bwimg.show('binaryimage.jpg')
+
+"""
+Hough circle
+"""
 
 
-# Create cv2 image object
-img = cv2.imread('binaryimage.png',0)
+# Open image as cv2 object
+bwimg = cv2.imread('binaryimage.jpg',0)
 
 
-# Blurs image with 5x5 aperture size (unnecessary?)
-# img = cv2.medianBlur(img,5)
+# Shrink image
+rsimg = cv2.resize(bwimg, None, fx = 0.25, fy = 0.25)
+
+cv2.imshow('Resized grayscale image',rsimg)	## Delete after testing
+cv2.imwrite('rsimg.jpg',rsimg)
+
+# Dilate image
+kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE,(100,100))	## Still figuring out what size to make kernels
+dimg = cv2.dilate(rsimg,kernel,iterations=1)
+
+cv2.imshow('Dilated image',dimg)	## Delete after testing
+cv2.imwrite('dimg.jpg',dimg)
+
+# Color conversion from grayscale to BGR
+cimg = cv2.cvtColor(dimg,cv2.COLOR_GRAY2BGR)
+
+cv2.imshow('BGR image',cimg)	## Delete after testing
+cv2.imwrite('cimg.jpg',cimg)
+
+# Find circles
+circles = cv2.HoughCircles(dimg,cv2.HOUGH_GRADIENT,1,20,
+							param1=50,param2=30,minRadius=0,maxRadius=0)
 
 
-# BGR to grayscale color conversion (unnecessary?)
-# cimg = cv2.cvtColor(img,cv2.COLOR_GRAY2BGR)
-
-
-#
-circles = cv2.HoughCircles(img,cv2.HOUGH_GRADIENT,1,20,
-                            param1=50,param2=30,minRadius=0,maxRadius=0)
-
+# Display circles and centers on resized grayscale image
 circles = np.uint16(np.around(circles))
 for i in circles[0,:]:
     # draw the outer circle
-    cv2.circle(cimg,(i[0],i[1]),i[2],(0,255,0),2)
+    cv2.circle(rsimg,(i[0],i[1]),i[2],(255,0,0),2)
     # draw the center of the circle
-    cv2.circle(cimg,(i[0],i[1]),2,(0,0,255),3)
+    cv2.circle(rsimg,(i[0],i[1]),2,(0,255,0),3)
 
-cv2.imshow('detected circles',cimg)
+cv2.imshow('detected circles',rsimg)	## Delete after testing
+cv2.imwrite('circles.jpg',rsimg)
+
+# 
+
+
+# Display images	## Delete after testing
 cv2.waitKey(0)
 cv2.destroyAllWindows()
