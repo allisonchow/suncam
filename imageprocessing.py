Updates for 9/27 test: fixed zenith degrees to rotate calculation, records more test data, updated image names, added messages for when image processing happening

imageprocessing.py
@@ -30,8 +30,17 @@ def sun_center(img):
     	Number of pixels in the y-direction between sun center and image center    	
 
     check : float
-    	Equals 0 if sun center is not detected. Equals 1 if sun center is detected.  	
+    	>= 0 if sun center is not detected. Equals -1 if sun center is detected.
+    	-1: sun center detected
+    	0: still looping through threshold values
+    	1: area of cluster too small to be sun center.
+    	2: not enough clusters detected.
+    	3: image not taken. 	
 	"""
+
+	print ("""
+		Beginning Image Processing!
+		""")
 
 	# Set checkmark value
 	check = 0	# If <0, then sun center detected. Else, then sun center not detected. 
@@ -63,11 +72,12 @@ def sun_center(img):
 		img_sun[np.where((r > 0.96) & (g > 0.96) & (b > 0.96) & (h > 0.78) & (s > j))] = 0
 
 
-		cv2.imwrite('/home/suncam/fswebcampics/{0} Binary THRESH {1}.jpg'.format(img,j), img_sun)	# Delete after testing
+		cv2.imwrite('/home/suncam/fswebcampics/{0} Binary.jpg'.format(img), img_sun)	# Delete after testing
 
 
 		# Label clustersd
 		lw, num = measurements.label(img_sun)
+		clustnum = len(lw)
 
 		# Makes sure that clusters are detected
 		if num > 1:
@@ -75,9 +85,10 @@ def sun_center(img):
 			# Find area of clusters
 			area = measurements.sum(img_sun, lw, index=arange(len(lw)))
 			area[np.where(area == np.amax(area))] = 0
+			a = area[np.argmax(area)]
 
 			# Makes sure that detected clusters are correct
-			if area[np.argmax(area)] > 50000:
+			if a > 50000:
 
 				# Find center of mass
 				[com_row, com_col] = np.round(measurements.center_of_mass(img_sun, lw, np.argmax(area)))
@@ -89,7 +100,7 @@ def sun_center(img):
 				img_bgr = cv2.line(img_bgr, (np.int(com_col + 1 - 10), np.int(com_row + 1)), (np.int(com_col + 1 + 10), np.int(com_row + 1)), [255,0,0], 1, 8)
 				img_bgr = cv2.line(img_bgr, (np.int(com_col + 1), np.int(com_row + 1 - 10)), (np.int(com_col + 1), np.int(com_row + 1 + 10)), [255,0,0], 1, 8)
 
-				cv2.imwrite('/home/suncam/fswebcampics/{0} Center THRESH {1}.jpg'.format(img,j), img_bgr)	# Delete after testing
+				cv2.imwrite('/home/suncam/fswebcampics/{0} Center {1}.jpg'.format(img,j), img_bgr)	# Delete after testing
 
 				check = -1
 
@@ -109,10 +120,16 @@ def sun_center(img):
 	if check >= 0:
 		dist_x = 0
 		dist_y = 0
+		j = 0
+		a = 0
+		clustnum = 0
 
+	print ("""
+	Completing Image Processing! Status: {0}
+	""".format(check))
 
 	# Return values
-	return [dist_x, dist_y, check]
+	return [dist_x, dist_y, check, j, a, clustnum]
 
 
 def rotate_center(dist_x, dist_y):
@@ -157,6 +174,6 @@ def rotate_center(dist_x, dist_y):
 	degree_a = math.degrees(math.atan(2*dist_x*math.tan(58/2)/1920))
 
     # Calculate zenith degrees to rotate
-	degree_z = math.degrees(math.atan(2*dist_y*math.tan(32.625/2)/1080))
+	degree_z = math.degrees(math.atan(2*dist_x*math.tan(58/2)/1920))
 
 	return [degree_a, degree_z]

suncam_img.py
@@ -28,12 +28,15 @@ total_moved_a = 0.0
 total_moved_z = 0.0
 reset = 1.0     # if reset, =1; if not reset, =0
 
+thresh = 44
+
 
 # Update settings
 gmt = timedelta(hours = 7) #-----1. time difference between location and GMT/UTC (depends on daylight savings)-----#
 dt = datetime.utcnow() - gmt
+start = dt
 # dt = datetime(2017, 7, 10)
-end = dt + timedelta(days = 4)  #-----2. duration of tracking-----#
+end = dt + timedelta(days = 5)  #-----2. duration of tracking-----#
 # end = datetime(2017, 7, 11)
 step = timedelta(minutes = 5)  #-----3. tracking intervals-----#
 result = []
@@ -41,7 +44,7 @@ result = []
 
 # Initialize Stepper
 stepper_z = ed.easydriver("P8_11", 0.007, "P8_12")
-stepper_a = ed.easydriver("P8_17", 0.01, "P8_18")
+stepper_a = ed.easydriver("P8_17", 0.007, "P8_18")
 
 #stepper_z.set_sixteenth_step()
 #stepper_a.set_sixteenth_step()
@@ -54,15 +57,13 @@ while dt <= end:
 
 
 # Initialize dataframe
-df = pd.DataFrame(columns = ['Timestamp', 'Calculated Elevation', 'Change in Elevation', 'Calculated Azimuthal', 'Change in Azimuthal', 'Horizontal Offset', 'Vertical Offset'])
+df = pd.DataFrame(columns = ['Timestamp', 'Calculated Elevation', 'Calculated Azimuthal', 'Change in Elevation', 'Change in Azimuthal', 'Horizontal Offset', 'Vertical Offset', 'Check', 'Threshold', 'Cluster Area', 'Number of Clusters', 'Loops'])
 
-df.to_csv('{0}_testdata.csv'.format(dt.strftime("%Y%m%d")))
+df.to_csv('{0}_test.csv'.format(start.strftime("%Y%m%d_%H%M%S")))
 
 
 # Iterate at each time
 for i in range(0, (len(result))):
-
-    print 'Iteration: {0}'.format(i)    ## remove after testing
 
     # Calculate current time and position
     ts = pd.Timestamp(result[i])
@@ -71,19 +72,26 @@ for i in range(0, (len(result))):
     elevation = angles.elevation_angle(ts + gmt, 32.879609, -117.235108)
     img = ts.strftime("%Y%m%d_%H%M%S") 
 
-    # Initialize values
+    # Null dataframe variables
+    k = 0   # Loop Counter
+    degree_a = 0
+    degree_z = 0
     dist_x = 0
     dist_y = 0
+    check = 3   # image not taken
+    satthresh = 0
+    clustarea = 0
+    clustnum = 0
 
 
-    # If more than half a step behind schedule, skip this iteration
+    # If more than half a step behind schedule, skip this iteration and set null data
     if (datetime.utcnow() - gmt) > (ts + (step/2)): 
 
-        print 'Could not capture image at {0}'.format(img)
+        print ('Could not capture image at {0}'.format(img))
 
         # Null dataframe variables
-        d_angle_z = 'NA'
-        d_angle_a = 'NA' 
+        d_angle_z = 0
+        d_angle_a = 0
 
         # Counter
         count += 1
@@ -92,7 +100,7 @@ for i in range(0, (len(result))):
         reset = 0
 
 
-    # If on schedule
+    # If on schedule, reposition, take picture, complete image analysis
     else:
 
 
@@ -109,10 +117,11 @@ for i in range(0, (len(result))):
             d_angle_z = elevation - now_angle_z # now_angle_z is actually elevation 
             d_angle_a = azimuth - now_angle_a
 
-            print '-------------Time: {0}---------------'.format(ts)
-            print '---------Actual Time: {0}---------'.format(datetime.utcnow().strftime('%Y-%m-%d %H-%M-%S'))  ## remove after testing
-            print 'Angle Difference Z = {0}'.format(d_angle_z)
-            print 'Angle Difference A = {0}'.format(d_angle_a)
+            print ("""
+
+                -------------Time: {0}---------------""".format(ts))
+            print ('Angle Difference Z = {0}'.format(d_angle_z))
+            print ('Angle Difference A = {0}'.format(d_angle_a))
 
             # Move stepper
             d_angle_z = stepper_z.rotate(d_angle_z) 
@@ -125,8 +134,8 @@ for i in range(0, (len(result))):
             total_moved_z = total_moved_z + d_angle_z
             total_moved_a = total_moved_a + d_angle_a
 
-            print 'Updated Elevation = {0}'.format(now_angle_z) 
-            print 'Updated Azimuth = {0}'.format(now_angle_a)
+            print ('Updated Elevation = {0}'.format(now_angle_z)) 
+            print ('Updated Azimuth = {0}'.format(now_angle_a))
 
             # Take picture
             os.system(
@@ -138,47 +147,44 @@ for i in range(0, (len(result))):
             if os.path.isfile('/home/suncam/fswebcampics/{0}.jpg'.format(img)) == True:
 
                 # Find sun center using image processing
-                [dist_x, dist_y, check] = sun_center(img)
+                [dist_x, dist_y, check, satthresh, clustarea, clustnum] = sun_center(img)
 
-                if check == 1:  # Sun center detected
+                # Feedback loop
+                while (np.fabs(dist_x) > thresh or np.fabs(dist_y) > thresh) and check == -1:
 
-                    # Feedback Loop: Rotate motor
-                    thresh = 44
-                    k = 1   # Counter
+                    k += 1
 
+                    # Calculate degrees to rotate
+                    [degree_a, degree_z] = rotate_center(dist_x, dist_y)
+
+                    # Rotate azimuthal motor
+                    if np.absolute(dist_x) > thresh:
+                        degree_a = stepper_a.rotate(degree_a)
+
+                    # Rotate zenith motor
+                    if np.absolute(dist_y) > thresh:
+                        degree_z = stepper_z.rotate(degree_z)
+
+                    # Re-initialize image proc values
                     degree_a = 0
                     degree_z = 0
+                    dist_x = 0
+                    dist_y = 0
+                    check = 3   # image not taken
+                    satthresh = 0
+                    clustarea = 0
+                    clustnum = 0
 
-                    while np.fabs(dist_x) > thresh or np.fabs(dist_y) > thresh and check == 1:
+                    # Take picture
+                    os.system(
+                        "fswebcam --jpeg 100 -D 2 -F 20 -S 5 -r 1920x1080 --flip v,h '/home/suncam/fswebcampics/{0} LOOP {1}.jpg'".format(img, k)
+                    )
 
-                        # Calculate degrees to rotate
-                        [degree_a, degree_z] = rotate_center(dist_x, dist_y)
+                    # Make sure picture was taken
+                    if os.path.isfile('/home/suncam/fswebcampics/{0} LOOP {1}.jpg'.format(img, k)) == True:
 
-                        # Rotate azimuthal motor
-                        if np.absolute(dist_x) > thresh:
-                            degree_a = stepper_a.rotate(degree_a)
-
-                        # Rotate zenith motor
-                        if np.absolute(dist_y) > thresh:
-                            degree_z = stepper_z.rotate(degree_z)
-
-                        # Take picture
-                        os.system(
-                            "fswebcam --jpeg 100 -D 2 -F 20 -S 5 -r 1920x1080 --flip v,h '/home/suncam/fswebcampics/{0} LOOP {1}.jpg'".format(img, k)
-                        )
-
-                        # Make sure picture was taken
-                        if os.path.isfile('/home/suncam/fswebcampics/{0} LOOP {1}.jpg'.format(img, k)) == True:
-
-                            # Find new sun center
-                            [dist_x, dist_y, check] = sun_center(("{0} LOOP {1}".format(img, k)))
-
-                        else:
-                            dist_x = 0
-                            dist_y = 0
-                            check = 0
-
-                        k += 1
+                        # Find new sun center
+                        [dist_x, dist_y, check, satthresh, clustarea, clustnum] = sun_center(("{0} LOOP {1}".format(img, k)))
 
 
             # Counter
@@ -192,8 +198,9 @@ for i in range(0, (len(result))):
         else:
 
             # Null dataframe variables
-            d_angle_z = 'NA'
-            d_angle_a = 'NA'
+            d_angle_z = 0
+            d_angle_a = 0
+
 
             # Counter
             count = 0
@@ -202,7 +209,7 @@ for i in range(0, (len(result))):
             # Needs to be reset
             if reset == 0:
                 
-                print 'It is night!'
+                print ('It is night!')
 
                 d_angle_a = stepper_a.rotate(-total_moved_a)
                 d_angle_z = stepper_z.rotate(-total_moved_z)
@@ -210,21 +217,19 @@ for i in range(0, (len(result))):
                 total_moved_a = total_moved_a + d_angle_a
                 total_moved_z = total_moved_z + d_angle_z
 
-                dist_x = 0
-                dist_y = 0
-
-
                 reset = 1   
 
-                print 'I have reset!'
+                print ('I have reset!')
 
 
     # Save information into dataframe
-    df = pd.read_csv('{0}_testdata.csv'.format(dt.strftime("%Y%m%d")), index_col = 0)
+    df = pd.read_csv('{0}_test.csv'.format(start.strftime("%Y%m%d_%H%M%S")), index_col = 0)
     
-    df.loc[len(df)] = [ts, elevation, d_angle_z, azimuth, d_angle_a, dist_x, dist_y] 
+
+    # ['Timestamp', 'Calculated Elevation', 'Calculated Azimuthal', 'Change in Elevation', 'Change in Azimuthal', 'Horizontal Offset', 'Vertical Offset', 'Check', 'Threshold', 'Cluster Area', 'Number of Clusters']
+    df.loc[len(df)] = [ts, elevation, azimuth, d_angle_z, d_angle_a, dist_x, dist_y, check, satthresh, clustarea, clustnum, k] 
     
-    df.to_csv('{0}_testdata.csv'.format(dt.strftime("%Y%m%d")))
+    df.to_csv('{0}_test.csv'.format(start.strftime("%Y%m%d_%H%M%S")))
 
 
     # Determines how long to sleep
@@ -252,4 +257,4 @@ for i in range(0, (len(result))):
         pd.set_option('display.max_rows', len(df))  ## remove after testing
         print(df)
 
-        print '--------------Ending solar tracking at {0}--------------'.format(datetime.utcnow().strftime('%Y-%m-%d %H-%M-%S'))
+        print ('--------------Ending solar tracking at {0}--------------'.format(datetime.utcnow().strftime('%Y-%m-%d %H-%M-%S')))